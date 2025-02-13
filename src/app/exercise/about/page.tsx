5add8724d38dbad7786afae1d3e359a2cf8a01c1
cellouRetailShake <cellu@retailshake.com>
2025-02-13 16:05:19 +0100

    refactor: clean up code and improve component structure

--- src/app/exercise/about/page.tsx
+++ src/app/exercise/about/page.tsx
@@ -1,6 +1,6 @@
 //export {default} from "./page.final"
 
-export {default} from './page.exercise'
+export { default } from "./page.exercise"
 
 // 1. 🚀 Convertir le composant en RCC (React Client Component)
-//export {default} from './page.bonus-1'
+//export { default } from "./page.bonus-1"
