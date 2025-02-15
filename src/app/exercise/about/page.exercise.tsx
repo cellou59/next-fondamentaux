1ae2480385a836e47d46fd5fa08edb506b10b1c8
cellouRetailShake <cellu@retailshake.com>
2025-02-15 12:21:11 +0100

    enhance admin styling in exercise page

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link'
+import clsx from 'clsx'
+
 import styles from './page.module.css'
 const Page = () => {
+  const isAdmin = Math.random() > 0.5
   return (
     <div>
       <p className={styles.title}>Je suis un développeur</p>
-      <p className="title">FullStack Next</p>
+      <p
+        className={clsx(
+          'inline-flex items-center',
+          isAdmin ? 'text-red-500' : 'text-green-500'
+        )}
+      >
+        FullStack Next
+      </p>
       <div className="mt-4">
         <Link
           className="rounded border border-gray-400 bg-white px-4 py-2 font-bold text-gray-800 hover:bg-gray-100"
