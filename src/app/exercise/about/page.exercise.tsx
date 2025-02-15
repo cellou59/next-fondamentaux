548061e1df5966b22f5b8825a93d7d4c978024fc
cellouRetailShake <cellu@retailshake.com>
2025-02-15 11:42:13 +0100

    enhance styling and structure of the exercise page

--- src/app/exercise/about/page.exercise.tsx
+++ src/app/exercise/about/page.exercise.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link'
-
+import styles from './page.module.css'
 const Page = () => {
   return (
     <div>
-      {/* 🐶 Applique le style global `.title` sur l'élément <p> */}
-      <p>Je suis un développeur FullStack Next</p>
-      {/* 🐶 Applique le style `tailwind` sur le composant `<Link>` pour le transformer en bouton*/}
-      <Link href="/exercise/account">Account</Link>
-      {/* 🐶 Tu peux wrapper d'une `div` avec 'mt-4' pour ajouter un margin top sur le button*/}
-
+      <p className={styles.title}>Je suis un développeur</p>
+      <p className="title">FullStack Next</p>
+      <div className="mt-4">
+        <Link
+          className="rounded border border-gray-400 bg-white px-4 py-2 font-bold text-gray-800 hover:bg-gray-100"
+          href="/exercise/account"
+        >
+          Account
+        </Link>
+      </div>
       {/* 🐶 Pense à regarder l'exercice bonus*/}
     </div>
   )
